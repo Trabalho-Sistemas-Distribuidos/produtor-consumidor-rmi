2620e3f287c174a253b6f7edab58c0c35863b2f8
petrolifero <jp_abreu@id.uff.br>
2018-05-21 14:54:06 -0300

    Put Consumer in a endless loop
    400 milisseconds is arbitrary, is the best? How will I write in the correct file? I need to contact the server? Ou manage my file by itself?

--- src/server/Consumer.java
+++ src/server/Consumer.java
@@ -6,6 +6,16 @@ import util.Buffer;
 
 pulic class Consumer{
   public int get() throws IllegalStateException{
-    return Buffer.get();
+    while(True){
+      try{
+        String a = Buffer.get();
+        //TODO : write in a corresponding file
+      }
+      catch(Exception e){
+        Thread.sleep(400); //wait 400 milisseconds;
+        
+      }
+    }
+    
   }
 }
