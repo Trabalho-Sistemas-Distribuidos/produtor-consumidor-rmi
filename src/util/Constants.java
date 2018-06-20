8069e8b28a5c7294c5d8887e67edb298c30aec96
Nicholas Campos Barcelos <nicholascb@id.uff.br>
2018-06-20 16:03:47 -0300

    Adiciona modificação q n tinha ido

--- src/util/Constants.java
+++ src/util/Constants.java
@@ -4,7 +4,7 @@ public class Constants {
     /** Tamanho do buffer do produtor/consumidor */
     public static final int BUFFER_SIZE = 3;
     /** Quantidade de clientes que o servidor atende */
-    public static final int SERVER_CAPACITY = 5;
+    public static final int SERVER_CAPACITY = 8;
     /** Quantidade de impressoras usadas no problema */
     public static final int PRINTERS_SIZE = 2;
     /** Nome do serviço do servidor */
